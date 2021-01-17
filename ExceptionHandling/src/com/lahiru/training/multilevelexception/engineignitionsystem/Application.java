02e2468ab24f1aa0e14235dc64a002e6c430643c
Lahiru Sandaruwan <lahirus250@gmail.com>
2021-01-17 18:00:25 +0530

    add checkCarStatus to main method

--- ExceptionHandling/src/com/lahiru/training/multilevelexception/engineignitionsystem/Application.java
+++ ExceptionHandling/src/com/lahiru/training/multilevelexception/engineignitionsystem/Application.java
@@ -1,12 +1,19 @@
 package com.lahiru.training.multilevelexception.engineignitionsystem;
 
+import com.lahiru.training.multilevelexception.exception.CarIsNotStartedException;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Application application = new Application();
+        application.checkCarStatus();
+    }
+
+    public void checkCarStatus() throws Exception {
         try {
             Car car = new Car();
             car.startCar();
-        } catch (Exception e){
+        } catch (CarIsNotStartedException e){
             e.printStackTrace();
         }
     }
