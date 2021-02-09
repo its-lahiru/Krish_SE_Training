927c9089abfcac7b3e3d89e5b435605dd4d6b442
Lahiru Sandaruwan <lahirus250@gmail.com>
2021-02-09 13:43:51 +0530

    first commit

---
+++ MicroserviceExamples/Vid9AuthorizationServer/src/main/java/com/lahiru/training/librarycloud/authorizationserver/model/Role.java
@@ -0,0 +1,26 @@
+package com.lahiru.training.librarycloud.authorizationserver.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "role")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "permission_role", joinColumns = {
+            @JoinColumn(name = "role_id", referencedColumnName = "id")}, inverseJoinColumns = {
+            @JoinColumn(name = "permission_id", referencedColumnName = "id")})
+    private List<Permission> permissions;
+
+}
---
+++ MicroserviceExamples/Vid9AuthorizationServer/src/main/java/com/lahiru/training/librarycloud/authorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,14 @@
+package com.lahiru.training.librarycloud.authorizationserver.repository;
+
+import com.lahiru.training.librarycloud.authorizationserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDetailRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String name);
+
+}
